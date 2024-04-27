35bbddac47eb8c9ef080404512a9cee4e9d22694
*Vp5KG20TDF <mnhat.nguyen@rikai.technology>
2024-04-27 15:43:22 +0700

    add some frontend

--- src/main/java/Website/LaptopShop/Controller/AdminController.java
+++ src/main/java/Website/LaptopShop/Controller/AdminController.java
@@ -108,6 +108,6 @@ public class AdminController {
         return "admin/manageAccount";
     }
 
-    @GetMapping("/statistical")
-    public String statisticalPage(Model model) { return "admin/statistical";}
+    @GetMapping("/statistic")
+    public String statisticalPage(Model model) { return "admin/statistic";}
 }
