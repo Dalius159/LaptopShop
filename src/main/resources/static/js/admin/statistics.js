ad38014a6e8ac35d8cb1477e629856cfd39f1625
AnKhanhh <93918878+AnKhanhh@users.noreply.github.com>
2024-04-29 01:12:00 +0700

    statistics

--- src/main/resources/static/js/admin/statistics.js
+++ src/main/resources/static/js/admin/statistics.js
@@ -1,9 +1,6 @@
 import {Chart} from "../chart.min";
 
 window.onload = function () {
-    let data = [];
-    let label = [];
-    let dataForDataSets = [];
     fetch("http://localhost:8080/laptopshop/api/order/report")
         .then(response => {
             if (!response.ok) {
@@ -12,55 +9,57 @@ window.onload = function () {
             return response.json();
         })
         .then(data => {
-                for (let i = 0; i < data.length; i++) {
-                    label.push(data[i][0] + "/" + data[i][1]);
-                    dataForDataSets.push(data[i][2] / 1000000);
-                }
+            let label = [];
+            let dataForDataSets = [];
+
+            for (let i = 0; i < data.length; i++) {
+                label.push(data[i][0] + "/" + data[i][1]);
+                dataForDataSets.push(data[i][2] / 1000000);
             }
-        )
+
+            const canvas = document.getElementById('myChart');
+            const chartData = {
+                labels: label,
+                datasets: [{
+                    label: "Total value",
+                    backgroundColor: "#0000ff",
+                    borderColor: "#0000ff",
+                    borderWidth: 2,
+                    hoverBackgroundColor: "#0043ff",
+                    hoverBorderColor: "#0043ff",
+                    data: dataForDataSets,
+                }]
+            };
+            const options = {
+                scales: {
+                    yAxes: [{
+                        stacked: true,
+                        gridLines: {
+                            display: true,
+                            color: "rgba(255,99,132,0.2)"
+                        }
+                    }],
+                    xAxes: [{
+                        barPercentage: 0.5,
+                        gridLines: {display: false}
+                    }]
+                },
+                maintainAspectRatio: false,
+                legend: {
+                    labels: {
+                        fontSize: 20 // override global
+                    }
+                }
+            };
+
+            new Chart(canvas.getContext('2d'), {
+                type: 'bar',
+                data: chartData,
+                options: options
+            });
+
+        })
         .catch(error => {
             console.error('Error:', error);
         });
-
-    const canvas = document.getElementById('myChart');
-    data = {
-        labels: label,
-        datasets: [{
-            label: "Total value",
-            backgroundColor: "#0000ff",
-            borderColor: "#0000ff",
-            borderWidth: 2,
-            hoverBackgroundColor: "#0043ff",
-            hoverBorderColor: "#0043ff",
-            data: dataForDataSets,
-        }]
-    };
-    const options = {
-        scales: {
-            yAxes: [{
-                stacked: true,
-                gridLines: {
-                    display: true,
-                    color: "rgba(255,99,132,0.2)"
-                }
-            }],
-            xAxes: [{
-                barPercentage: 0.5,
-                gridLines: {display: false}
-            }]
-        },
-        maintainAspectRatio: false,
-        legend: {
-            labels: {
-                fontSize: 20	// override global
-            }
-        }
-    };
-
-    const barChart = new Chart(canvas.getContext('2d'), {
-        type: 'bar',
-        data: data,
-        options: options
-    });
 }
-
