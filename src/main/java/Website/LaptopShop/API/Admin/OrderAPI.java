625cdf5fa576b1e11577e2c45a3e0b149b5e41f8
AnKhanhh <93918878+AnKhanhh@users.noreply.github.com>
2024-05-01 14:09:22 +0700

    upd

--- src/main/java/Website/LaptopShop/API/Admin/OrderAPI.java
+++ src/main/java/Website/LaptopShop/API/Admin/OrderAPI.java
@@ -20,81 +20,79 @@ import java.util.List;
 @RequestMapping("/api/order")
 public class OrderAPI {
 
-	@Autowired
-	private OrderService orderService;
+    @Autowired
+    private OrderService orderService;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	//Get orderList by search object
-	@GetMapping("/all")
-	public Page<Orders> getOrderByFilter(@RequestParam(defaultValue = "1") int page, @RequestParam String status,
-										 @RequestParam String fromDate, @RequestParam String toDate) throws ParseException {
+    //Get orderList by search object
+    @GetMapping("/all")
+    public Page<Orders> getOrderByFilter(@RequestParam(defaultValue = "1") int page, @RequestParam String status,
+                                         @RequestParam String fromDate, @RequestParam String toDate) throws ParseException {
 
-		SearchOrderObject object = new SearchOrderObject();
-		object.setToDate(toDate);
-		object.setOrderStatus(status);
-		object.setFromDate(fromDate);
-		return orderService.getAllOrderByFilter(object, page);
-	}
+        SearchOrderObject object = new SearchOrderObject();
+        object.setToDate(toDate);
+        object.setOrderStatus(status);
+        object.setFromDate(fromDate);
+        return orderService.getAllOrderByFilter(object, page);
+    }
 
-	@GetMapping("/{id}")
-	public Orders getOrderById(@PathVariable long id) {
-		return orderService.findById(id);
-	}
+    @GetMapping("/{id}")
+    public Orders getOrderById(@PathVariable long id) {
+        return orderService.findById(id);
+    }
 
-	// order assignment
-	@PostMapping("/assign")
-	public void orderAssignment(@RequestParam("deliverEmail") String deliverEmail,
-								@RequestParam("orderID") long orderID) {
-		Orders dh = orderService.findById(orderID);
-		dh.setOrderStatus("Delivering");
-		dh.setDeliver(userService.findByEmail(deliverEmail));
+    // order assignment
+    @PostMapping("/assign")
+    public void orderAssignment(@RequestParam("deliverEmail") String deliverEmail,
+                                @RequestParam("orderID") long orderID) {
+        Orders dh = orderService.findById(orderID);
+        dh.setOrderStatus("Delivering");
+        dh.setDeliver(userService.findByEmail(deliverEmail));
 
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		try {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
 
-			String dateStr = format.format(new Date());
-			Date date = format.parse(dateStr);
-			dh.setDeliveryDate(date);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+            String dateStr = format.format(new Date());
+            Date date = format.parse(dateStr);
+            dh.setDeliveryDate(date);
+        } catch (ParseException e) {e.printStackTrace();}
 
-		orderService.save(dh);
-	}
+        orderService.save(dh);
+    }
 
-	// Order completed confirm
-	@PostMapping("/update")
-	public void orderComplete(@RequestParam("orderID") long orderID,
-							  @RequestParam("adminNote") String adminNote) {
-		Orders dh = orderService.findById(orderID);
+    // Order completed confirm
+    @PostMapping("/update")
+    public void orderComplete(@RequestParam("orderID") long orderID,
+                              @RequestParam("adminNote") String adminNote) {
+        Orders dh = orderService.findById(orderID);
 
-		for (OrderDetails ct : dh.getOrderDetailsList()) {
-			Product sp = ct.getProduct();
-			sp.setSalesUnit(sp.getSalesUnit() + ct.getReceivedQuantity());
-			sp.setWarehouseUnit(sp.getWarehouseUnit() - ct.getReceivedQuantity());
-		}
-		dh.setOrderStatus("Completed");
-		String note = dh.getNote();
-		if (!adminNote.equals("")) {
-			note += "<br> Admin Note:\n" + adminNote;
-		}
-		dh.setNote(note);
-		orderService.save(dh);
-	}
+        for (OrderDetails ct : dh.getOrderDetailsList()) {
+            Product sp = ct.getProduct();
+            sp.setSalesUnit(sp.getSalesUnit() + ct.getReceivedQuantity());
+            sp.setWarehouseUnit(sp.getWarehouseUnit() - ct.getReceivedQuantity());
+        }
+        dh.setOrderStatus("Completed");
+        String note = dh.getNote();
+        if (!adminNote.equals("")) {
+            note += "<br> Admin Note:\n" + adminNote;
+        }
+        dh.setNote(note);
+        orderService.save(dh);
+    }
 
-	// Order canceled confirm
-	@PostMapping("/cancel")
-	public void orderCanceled(@RequestParam("orderID") long orderID) {
-		Orders dh = orderService.findById(orderID);
-		dh.setOrderStatus("Canceled");
-		orderService.save(dh);
-	}
+    // Order canceled confirm
+    @PostMapping("/cancel")
+    public void orderCanceled(@RequestParam("orderID") long orderID) {
+        Orders dh = orderService.findById(orderID);
+        dh.setOrderStatus("Canceled");
+        orderService.save(dh);
+    }
 
-	// Get data for statistical reports
-	@GetMapping("/report")
-	public List<Object> test() {
-		return orderService.getOrderByMonthAndYear();
-	}
+    // Get data for statistical reports
+    @GetMapping("/report")
+    public List<Object> test() {
+        return orderService.getOrderByMonthAndYear();
+    }
 }
